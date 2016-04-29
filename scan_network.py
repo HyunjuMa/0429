ce02e3137bfd43abf6c6c18166c2bed4e59bb619
HyunjuMa <d15123649@mydit.ie>
2016-04-29 13:09:48 +0900

    test

--- scan_network.py
+++ scan_network.py
@@ -3,7 +3,7 @@ import sys
 
 def scanning_ip(ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(0.5)
+    sock.settimeout(0.2)
     result = sock.connect_ex((ip, 80))
     
     if result == 0:
@@ -16,9 +16,10 @@ def scanning_ip(ip):
 
 
 res = []
-for ip1 in range(2,256):
-    for ip2 in range(0,256):
-        ip = "166.104." + str(ip1) + '.' + str(ip2)
+#for ip1 in range(2,256):
+#    for ip2 in range(0,256):
+#        ip = "166.104." + str(ip1) + '.' + str(ip2)
+		ip = "166.104.28.50"
         print('scanning: ' + ip)
         if scanning_ip(ip) == 'success':
             res.append(ip)
